303cab2f11af8f56eefa7c6a3a1ec6e516421adb
Joakim Nylén <git@joakim.nylen.nu>
2023-02-23 07:46:22 +0800

    fix

--- src/Field.php
+++ src/Field.php
@@ -19,6 +19,20 @@ class Field
     }
 
     /**
+     * Render an input[type=text] field
+     *
+     * @param array $args
+     * @return void
+     */
+    public static function string($args = [])
+    {
+        ?><div>
+            <input type="text" class="regular-text" name="<?= esc_attr($args['name']); ?>" value="<?= $args['value']; ?>">
+            <?= !empty($args['description']) ? "<p class=\"description\">{$args['description']}</p>" : ''; ?>
+        </div><?php
+    }
+
+    /**
      * Render a select field
      *
      * @param array $args
